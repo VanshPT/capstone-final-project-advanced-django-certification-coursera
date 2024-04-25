5574724995338a4ad1cadfd4aec4c40d8fb485a8
9580_Vansh_Thakkar 9580_Vansh_Thakkar <crce.9580.ce@gmail.com>
2024-04-25 19:54:01 +0000

    Finish scheduling with celery

--- movies/signals.py
+++ movies/signals.py
@@ -2,6 +2,8 @@ from django.core.signals import request_finished
 from django.dispatch import receiver
 from movies.models import SearchTerm
 from movies.tasks import notify_of_new_search_term
+from django.db.models.signals import post_save
+
 
 @receiver(post_save, sender=SearchTerm, dispatch_uid="search_term_saved")
 def search_term_saved(sender, instance, created, **kwargs):
